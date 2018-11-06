b68a1149cb5aa9e13891ca57e72241d56ba4dc16
DylanJansen <40144787+DylanJansen@users.noreply.github.com>
2018-11-06 16:19:06 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,64 @@
+const Discord = require('discord.js');
+const superagent = require("superagent");
+const Client = new Discord.Client();
+const OwnerID = "509385145732825099";
+
+const prefix = "!"
+
+
+
+Client.on("ready", () => {
+	console.log("online");
+	Client.user.setPresence({ game: { name: `Hello world`, type: 0} });
+});
+
+// welcome message
+
+Client.on("guildMemberAdd", member => {
+   member.guild.defaultChannel.send("Welcome to: " + member.guild.name + " dfghh")
+});
+
+Client.on("guildMemberRemove", member => {
+   member.guild.defaultChannel.send("Goodbye: " + member.user.username + " from " + member.guild.name)
+});
+
+Client.on("guildCreate", guild => {
+	console.log("Some one added the test bot to a server created by: " + guild.owner.user.username)
+});
+
+Client.on("message", async (message) => {
+	if (message.author.bot) return;
+	if (!message.content.startsWith(prefix)) return;
+	
+	let command = message.content.split(" ")[0];
+	command = command.slice(prefix.length);
+	
+	let args = message.content.split(" ").slice(1);
+	
+	if (command === "ping") {
+		message.channel.send(`Pong! Time took: ${Date.now() - message.createdTimestamp} ms`);
+	} else
+
+	if (command === "say") {
+		message.delete()
+        const embed = new Discord.RichEmbed()
+		.setColor(0x954D23)
+		.setDescription(message.author.username + " says: " + args.join(" "));
+		message.channel.send({embed})
+	} else
+
+	if (command == "help") {
+		const embed = new Discord.RichEmbed()
+		.setColor(0x954D23)
+		.setTitle("Command List:")
+		.addField("!help", "Will give the current command list")
+		.addField("!ping", "WIll show the ping time for the bot")
+		.addField("!say [text]", "Will make the bot say something")
+		.addField("!announcement [text]", "Will make the bot say an announcement and tag everyone")
+		.addField("!cat", "Will send a random cat image");
+		message.channel.send({embed})
+	}
+
+});
+
+Client.login("NTA5Mzg1MTQ1NzMyODI1MDk5.DsNBoQ.Qwm4pUuqmQIgRAz8unz6hNU2lok");
